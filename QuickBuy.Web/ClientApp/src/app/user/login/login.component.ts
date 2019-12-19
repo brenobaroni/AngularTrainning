c0d566440301db1370e8e73b4d5856dc8a116c04
Breno Baroni <breno_baroni@hotmail.com>
2019-12-18 22:00:24 -0300

    Update in login Component.

--- QuickBuy.Web/ClientApp/src/app/user/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/user/login/login.component.ts
@@ -1,26 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { User } from "../../model/user";
-import { Router } from "@angular/router"
+import { Router, ActivatedRoute } from "@angular/router"
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   public user;
+  public returnUrl: string;
   public enderecoImg = "../assets/img/logo.png";
 
-  constructor(private router: Router) {
-    this.user = new User();
+  constructor(private router: Router, private activatedRouter: ActivatedRoute) {
   }
 
+  ngOnInit(): void {
+    this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
+    this.user = new User();
+  }
 
   enter() {
     if (this.user.email == "breno_baroni@hotmail.com" && this.user.password == "123Senha") {
       sessionStorage.setItem("user-authenticated", "1");
-      this.router.navigate(['/'])
+      this.router.navigate([this.returnUrl]);
     }
   }
 }
